1421b35d68cc31b17570012832fbb5aa3bd7c390
nadir chrair <chrairmohamednadir@gmail.com>
2022-05-28 16:42:27 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+let menubtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+menubtn.onclick = () => {
+    menubtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+var swiper = new Swiper(".courses-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    clickable: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+var swiper = new Swiper(".teachers-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    clickable: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    clickable: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
